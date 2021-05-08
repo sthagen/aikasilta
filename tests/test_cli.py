19a24c024c6de17c7df9cbd7c5860684a9a20491
Stefan Hagen <stefan@hagen.link>
2021-05-08 11:02:39 +0200

    Added first test to bootstrap the development

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,1 +1,17 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import logging
+import pathlib
 
+import pytest  # type: ignore
+
+import time_bridge.cli as cli
+
+
+def test_main_nok_bad_arg(capsys):
+    with pytest.raises(SystemExit, match="2"):
+        cli.main(["non-existing-thing"], debug=False)
+    out, err = capsys.readouterr()
+    assert "error" in out.lower()
+    assert "for now only existing paths accepted." in out.lower()
+    assert not err
